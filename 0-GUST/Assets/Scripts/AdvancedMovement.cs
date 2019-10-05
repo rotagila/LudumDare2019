9e70ab710f023ae9f516e0c2803ec4017081b69f
antoria <antoiria91@gmail.com>
2019-10-05 17:16:49 -0400

    Created basic movement, advanced movement and skills menu

---
+++ 0-GUST/Assets/Scripts/AdvancedMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedMovement : MonoBehaviour
+{
+    public float maxMovementSpeed;
+    public float acceleration;
+    public float deceleration;
+
+    private float currentSpeed;
+
+    enum MovementType
+    {
+        DECELERATION = 0,
+        ACCELERATION = 1
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Not moving
+        if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            ComputeCurrentSpeed(MovementType.DECELERATION);
+        }else
+        {// Moving
+            ComputeCurrentSpeed(MovementType.ACCELERATION);
+
+            if (Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.S)) // forward
+                transform.position += transform.TransformDirection(Vector3.up) * currentSpeed;
+            
+            if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.Z)) // backward
+                transform.position += transform.TransformDirection(Vector3.down) * currentSpeed;
+            
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey("q")) // right
+                transform.position += transform.TransformDirection(Vector3.right) * currentSpeed;
+             
+            if (Input.GetKey(KeyCode.Q) && !Input.GetKey("d")) // left
+                transform.position += transform.TransformDirection(Vector3.left) * currentSpeed;
+        }
+    }
+
+    void ComputeCurrentSpeed(MovementType mt)
+    {
+        if(mt == MovementType.DECELERATION)
+        {
+            currentSpeed -= deceleration * Time.deltaTime;
+            if (currentSpeed < 0.0f) currentSpeed = 0.0f;
+        }
+        else if(mt == MovementType.ACCELERATION)
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+            if (currentSpeed > maxMovementSpeed) currentSpeed = maxMovementSpeed;
+        }
+    }
+}
---
+++ 0-GUST/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject skillSelectionMenu;
+
+    public GameObject[] activeSkillsPanels;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if (isGamePaused) Resume(skillSelectionMenu);
+            else Pause(skillSelectionMenu);
+        }
+    }
+
+    public void ActivateSkill(GameObject clickedPanel)
+    {
+        string t = clickedPanel.GetComponentInChildren<Text>().text;
+
+        ResetPanelColors();
+
+        clickedPanel.GetComponent<Image>().color = new Color(0, 255, 0, 150);
+        
+
+    }
+
+    void ResetPanelColors()
+    {
+        if(activeSkillsPanels.Length > 0)
+        {
+            foreach (GameObject go in activeSkillsPanels)
+            {
+                go.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            }
+        }
+        
+    }
+
+    void Resume(GameObject menu)
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+    }
+
+    void Pause(GameObject menu)
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0.0f;
+        isGamePaused = true;
+    }
+}

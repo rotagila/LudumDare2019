cd9902101daa7d537404e7926e415146d49afae0
clement-ramat <balstik17@gmail.com>
2019-10-06 18:09:40 -0400

    added music prefab

---
+++ 0-GUST/Assets/Scripts/Environnement/MusicLoop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    public AudioClip startClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(startClip);
+        audioSource.PlayScheduled(AudioSettings.dspTime + startClip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

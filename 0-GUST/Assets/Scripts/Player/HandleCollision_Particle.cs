09bd5f9eaaff68d1182305220e2d3103090a507d
antoria <antoiria91@gmail.com>
2019-10-06 19:32:05 -0400

    Added Telescopic Arms & "Grabbable" tag

--- 0-GUST/Assets/Scripts/Player/HandleCollision_Particle.cs
+++ 0-GUST/Assets/Scripts/Player/HandleCollision_Particle.cs
@@ -31,7 +31,5 @@ public class HandleCollision_Particle : MonoBehaviour
 
             Destroy(iceCubesShatterEffect, iceCubesShatterEffect.GetComponent<ParticleSystem>().duration);
         }
-
-
     }
 }
--- 0-GUST/Assets/Scripts/Player/Skill_TelescopicArms.cs
+++ 0-GUST/Assets/Scripts/Player/Skill_TelescopicArms.cs
@@ -4,29 +4,84 @@ using UnityEngine;
 
 public class Skill_TelescopicArms : MonoBehaviour
 {
-    public float moveSpeed;
+    public float grappleSpeed;
+    public float playerSpeed;
+    public float maxDistance;
 
-    private bool effectActive;
+    public GameObject grapple;
+
+    private bool grappleLaunched;
+    private bool hooked;
+
+    private GameObject grappleInstance;
+
     private Vector3 targetPosition;
+    private Quaternion q; 
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !grappleLaunched && !hooked)
         {
-            effectActive = true;
+            grappleLaunched = true;
+            hooked = false;
+
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = transform.position.z;
 
-            Debug.Log("ini: " + targetPosition);
+            Vector3 direction = GetDirection();
+
+            q = Quaternion.LookRotation(direction);
+
+            grappleInstance = Instantiate(grapple, transform.position, Quaternion.identity);
+        
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            grappleInstance.transform.eulerAngles = new Vector3(0.0f, 0.0f, angle);
+
         }
 
-        if(effectActive)
+        if(grappleLaunched && (grappleInstance != null) && !hooked)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-            Debug.Log("pos: " + transform.position + " -- going to "+ targetPosition);
-            if (transform.position == targetPosition) 
-                effectActive = false;
+            grappleInstance.transform.position = Vector3.MoveTowards(grappleInstance.transform.position, targetPosition, grappleSpeed * Time.deltaTime);
+
+            if (grappleInstance.GetComponent<GrappleCollision>().GetIsColliding())
+            {
+                hooked = true;
+            }
+                
+
+            // if maxDistance reached OR target reached and nothing hooked
+            if ((Vector3.Distance(transform.position, grappleInstance.transform.position) >= maxDistance)
+                || ((grappleInstance.transform.position == targetPosition) && !hooked)
+                )
+                ResetSkill();
         }
+
+        if(hooked && (grappleInstance != null))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, grappleInstance.transform.position, playerSpeed * Time.deltaTime);
+
+            if(transform.position == grappleInstance.transform.position)
+                ResetSkill();  
+        }
+    }
+
+    Vector3 GetDirection()
+    {
+        return Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    void ResetSkill()
+    {
+        grappleLaunched = false;
+        hooked = false;
+        Destroy(grappleInstance);
+    }
+
+    public void FoundGrabbable()
+    {
+        Debug.Log("found grabbable object !");
     }
 
 }

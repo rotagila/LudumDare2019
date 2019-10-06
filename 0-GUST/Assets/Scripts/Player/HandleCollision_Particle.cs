d447f245cdd030f307e5aec7a37cdc0a1e2bcba6
antoria <antoiria91@gmail.com>
2019-10-06 12:46:25 -0400

    Fixed flamethrower, created EMP, updated HandleCollision script to update all particle collisions (currently flamethrower and EMP)

---
+++ 0-GUST/Assets/Scripts/Player/HandleCollision_Particle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleCollision_Particle : MonoBehaviour
+{
+
+    public Sprite burntSprite;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.name.Equals("ParticleSystem_Flamethrower(Clone)"))
+        {
+            Debug.Log("hot !");
+            GetComponent<SpriteRenderer>().sprite = burntSprite;
+        }
+        else if (other.name.Equals("ParticleSystem_Emp(Clone)"))
+        {
+            Debug.Log("emp'd");
+        }
+
+        
+    }
+}

501948ea99ad6e88cf0363dbfdde213cb8c4b485
clement-ramat <balstik17@gmail.com>
2019-10-06 12:45:49 -0400

    added scripts for vision in BasicCharacter

---
+++ 0-GUST/Assets/Scripts/Player/Skill_BasicVision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_BasicVision : MonoBehaviour
+{
+
+    public GameObject basicVision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        basicVision.transform.position = this.transform.position;
+    }
+
+    private void OnEnable()
+    {
+        basicVision = Instantiate(basicVision, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    private void OnDisable()
+    {
+        Destroy(basicVision);
+    }
+}

0ddcedce273b8e694de11bb5b0a11251fbbd9ef7
clement-ramat <balstik17@gmail.com>
2019-10-06 21:06:46 -0400

    alleluia mes frères

---
+++ 0-GUST/Assets/Test/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    int chasePlayerParamID;
+    FieldOfView fov;
+    Animator animator;
+    void Start()
+    {
+        chasePlayerParamID = Animator.StringToHash("ChasePlayer");
+        animator = GetComponent<Animator>();
+        fov = GetComponentInChildren<FieldOfView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(chasePlayerParamID, fov.chasePlayer);
+    }
+}
